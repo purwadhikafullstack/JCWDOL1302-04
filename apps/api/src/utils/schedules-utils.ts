85f5181c85a3fa17f8d2c38fbc059d9f94d72672
vyaninsyanurmuhammad <vyaninsya.nurmuhammad@gmail.com>
2024-07-15 18:48:19 +0700

    refactor: wrap functions with try and catch for safety

--- apps/api/src/utils/schedules-utils.ts
+++ apps/api/src/utils/schedules-utils.ts
@@ -4,85 +4,91 @@ import { addDays } from 'date-fns';
 import { templateRuleNodemailer } from '../../lib/nodeMailer';
 
 export const reminderRuleUtil = async () => {
-  const orders = await prisma.order.findMany({
-    where: {
-      status: 'finished',
-      isReminded: false,
-    },
-    include: {
-      user: true,
-    },
-  });
+  try {
+    const orders = await prisma.order.findMany({
+      where: {
+        status: 'finished',
+        isReminded: false,
+      },
+      include: {
+        user: true,
+      },
+    });
 
-  for (const {
-    checkIn,
-    user: { email },
-  } of orders) {
-    if (
-      new Date().toDateString() ===
-      addDays(new Date(checkIn), -1).toDateString()
-    ) {
-      templateRuleNodemailer(email);
+    for (const {
+      checkIn,
+      user: { email },
+    } of orders) {
+      if (
+        new Date().toDateString() ===
+        addDays(new Date(checkIn), -1).toDateString()
+      ) {
+        templateRuleNodemailer(email);
+      }
     }
-  }
+  } catch (error) {}
 };
 
 export const updateExpiredBookingPayment = async () => {
-  const orders = await prisma.order.findMany({
-    where: {
-      status: 'pending',
-      expDateTime: {
-        lt: new Date(),
-      },
-    },
-  });
-
-  if (orders.length > 0) {
-    await prisma.order.updateMany({
+  try {
+    const orders = await prisma.order.findMany({
       where: {
         status: 'pending',
         expDateTime: {
           lt: new Date(),
         },
       },
-      data: {
-        status: 'expired',
-      },
     });
-  }
+
+    if (orders.length > 0) {
+      await prisma.order.updateMany({
+        where: {
+          status: 'pending',
+          expDateTime: {
+            lt: new Date(),
+          },
+        },
+        data: {
+          status: 'expired',
+        },
+      });
+    }
+  } catch (error) {}
 };
 
 export const updateConfirmingBookingPayment = async () => {
-  const orders = await prisma.order.findMany({
-    where: {
-      status: 'pending',
-      expDateTime: {
-        gt: new Date(),
+  try {
+    const orders = await prisma.order.findMany({
+      where: {
+        status: 'pending',
+        expDateTime: {
+          gt: new Date(),
+        },
       },
-    },
-  });
+    });
 
-  if (orders.length > 0) {
-    for (const { id, userId, invoiceId } of orders) {
-      const result = await TransactionService.checkDOKUPayment({
-        userId,
-        invoiceId,
-      });
-
-      if (result && result.transaction.status === 'SUCCESS') {
-        await prisma.order.update({
-          where: {
-            id,
-            status: 'pending',
-            expDateTime: {
-              gt: new Date(),
-            },
-          },
-          data: {
-            status: 'confirming',
-          },
+    if (orders.length > 0) {
+      for (const { id, userId, invoiceId } of orders) {
+        const result = await TransactionService.checkDOKUPayment({
+          userId,
+          invoiceId,
         });
+
+        if (result && result.transaction.status === 'SUCCESS') {
+          await prisma.order.update({
+            where: {
+              id,
+              status: 'pending',
+              expDateTime: {
+                gt: new Date(),
+              },
+            },
+            data: {
+              status: 'confirming',
+            },
+          });
+        }
       }
     }
-  }
+  } catch (error) {}
 };
